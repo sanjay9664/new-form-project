3fff02e996ec9a1fdf281f0a00302ea3f20af507
sanjay gupta <guptaji30749@gmail.com>
2022-12-21 08:45:39 +0530

    completeTast

--- new/src/App.js
+++ new/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddUser from "./component/User/AddUser"
+ import UserList from './component/User/UserList'
 function App() {
+   
+     const [userList , setUserList ] = useState([])
+
+     const addUserHandler = (uName, uAge)=>{
+      setUserList((preUserList) =>{
+        return [...preUserList , {name: uName,  age:uAge , id:Math.random().toString()},
+        ];
+      });
+     }
   return (
     <div>
-      <AddUser/>
+      <AddUser onAddUser={addUserHandler}/>
+     <UserList  users={userList}/>
     </div>
   );
 }
--- new/src/component/User/AddUser.js
+++ new/src/component/User/AddUser.js
@@ -1,28 +1,70 @@
 
-import React  from "react";
+import React, { useState }   from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css"
-const AddUser = props =>{
+import Button from "../UI/Button";
+import ErrorModel from "../UI/ErrorModel";
+const AddUser = (props) =>{
+
+     const[enterusername , setEnterusername] = useState('')
+     const [enterAge , setenterAge] = useState("")
+
+      const [error, setError] = useState("")
 
     const AdduserHandler = (event)=>{
-        event.preventDefault()
-        let form = document.getElementById("form")
-        let username = form.username.value 
-        let age = form.age.value 
-        console.log(username , age)
+        event.preventDefault(); 
+        
+        if(enterusername.trim().length===0 ||enterAge.trim().length===0){
 
+            setError({
+                title:"Invaild input",
+                message: "Please enter vaild name and age (non-Empty value)!"
+            })
+            return 
+            // alert("Enter something !")
+        }
+        
+        if(+enterAge<1){
+            setError({
+                title:"Invaild age",
+                message: "Please enter vaild age (> 0)"
+            })
+         return 
+        //  alert(" Enter more then -1 begger value like 1 !")
+        }
+        // console.log(enterusername,enterAge)
+        props.onAddUser(enterusername,enterAge)
+
+        setEnterusername("")
+        setenterAge("")
+    }
+      
+    const enteruserhandler = (event)=>{
+    setEnterusername(event.target.value)
     }
 
+    const enterAgehandler  = (event)=>{
+   setenterAge(event.target.value)
+    }
+
+       const errorhandler = ()=>{
+        setError(null)
+       }
+    
     return(
+        <div>
+        {error && <ErrorModel title={error.title} message={error.message} onConfirm={errorhandler}/>}
         <Card className={classes.input}>
-        <form  id="form" onSubmit={AdduserHandler}>
+        <form   onSubmit={AdduserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" placeholder="Enter your name" />
+            <input id="username" type="text" value={enterusername} onChange={enteruserhandler} placeholder="Enter your name" />
             <label htmlFor="age">Age (Year)</label>
-            <input id="age" type="text" placeholder="Enter your name" />
-            <button type="submit">Add User</button>
+            <input id="age" type="text" value={enterAge} onChange={enterAgehandler}  placeholder="Enter your name" />
+            <Button type="submit">Add User</Button>
         </form>
+        
         </Card>
+        </div>
     )
 }
 
